bf0c9a65ee7ac96214c66dca10155b8cca6a4b89
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-17 18:54:16 +0800

    create method to display the sorted elements

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -106,7 +106,27 @@ namespace QuickSort
             //sort the list on the right of pivot using quick sort
             q_sort(j + 1, high);
         }
+        void display()
+        {
+            Console.WriteLine("\n-------------------------");
+            Console.WriteLine(" sorted array elements ");
+            Console.WriteLine("---------------------------");
 
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of data movemenets: " + mov_count);
+        }
+        int getsize()
+        {
+            return (n);
+        }
+        static void main(string[] args)
+        {
+            //declaring the objec
+        }
     }
 }
 
