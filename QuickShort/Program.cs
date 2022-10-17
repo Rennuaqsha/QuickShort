5af76bd64387ca56a0860e9c5cad471a2af6e511
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-17 18:21:30 +0800

    create method to insert data element array

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -15,6 +15,34 @@ namespace QuickSort
         //private of elements in array
         private int n;
 
+        void read()
+        {
+            while (true)
+            {
+                Console.Write("Enter The Number Of Elements in The array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n====================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n====================");
+
+
+            //get array Elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+
+            }
+        }
+
+
     }
 }
 
