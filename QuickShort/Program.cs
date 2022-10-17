863810d8b1567dfe9c7436dec5561c87d594d08a
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-17 18:16:32 +0800

    create array and variabel int

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -1,6 +1,20 @@
-﻿namespace QuickSort
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+using System.Xml;
 
-class Program
+namespace QuickSort
 {
-    //array of integers to hold values
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int mov_count = 0; ////number of data  movements
+
+        //private of elements in array
+        private int n;
+
+    }
 }
+
